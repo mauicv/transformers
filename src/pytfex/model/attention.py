19abe51def630c70698dc48ded75a5b9a54320bb
Alex Athorne <a.thornysort@gmail.com>
2023-10-20 07:53:25 +0100

    Add mingpt attention implementation

--- src/pytfex/model/attention.py
+++ src/pytfex/model/attention.py
@@ -1,53 +1,73 @@
-import torch
+import math
 
-class Attention(torch.nn.Module):
-    def __init__(
-            self,
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class Config:
+    def __init__(self, n_embd, n_head, block_size) -> None:        
+        self.n_embd = n_embd
+        self.n_head = n_head
+        self.block_size = block_size
+        self.attn_pdrop = 0.01
+        self.resid_pdrop = 0.01
+
+
+class Attention(nn.Module):
+    """
+    A vanilla multi-head masked self-attention layer with a projection at the end.
+    It is possible to use torch.nn.MultiheadAttention here but I am including an
+    explicit implementation here to show that there is nothing too scary here.
+    """
+
+    def __init__(self, 
             hidden_dim: int,
             num_heads: int,
-            dropout: float=0.5,
-        ) -> None:
-        super(Attention, self).__init__()
+            dropout: float=0.5
+        ):
+        super().__init__()
         self.hidden_dim = hidden_dim
         self.num_heads = num_heads
-        self.dropout = torch.tensor(
-            dropout,
-            dtype=torch.float32
-        )
-        self.attn_dropout = torch.nn.Dropout(self.dropout)
-        self.resid_dropout = torch.nn.Dropout(self.dropout)
+        self.dropout = dropout
 
-        self.qkv = torch.nn.Linear(
-            self.hidden_dim,
-            3 * self.hidden_dim
+        config = Config(
+            n_embd=hidden_dim,
+            n_head=num_heads,
+            block_size=256,
         )
-        self.linear = torch.nn.Linear(
-            self.hidden_dim,
-            self.hidden_dim
-        )
+        
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.attn_pdrop)
+        self.resid_dropout = nn.Dropout(config.resid_pdrop)
+        # causal mask to ensure that attention is only applied to the left in the input sequence
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                     .view(1, 1, config.block_size, config.block_size))
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
 
-        self.head_dim = self.hidden_dim // self.num_heads
+    def forward(self, x, mask=None):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
-    def forward(
-                self,
-                x,
-                mask=None
-            ):
-        if mask is not None:
-            assert mask.dtype == torch.bool, "Mask must be of type torch.float32"
-        assert len(x.shape) == 3, "Input must have shape (batch, seq_len, hidden_dim)"
-        assert x.shape[-1] == self.hidden_dim, "Input has incorrect hidden_dim"
-        b, l, d = x.shape
-        qkv = self.qkv(x)
-        qkv = qkv.reshape(b, 3, self.num_heads, l, -1)
-        q, k, v = torch.split(qkv, [1, 1, 1], dim=1)
-        q, k, v = q.squeeze(1), k.squeeze(1), v.squeeze(1)
-        a = k @ q.transpose(-2, -1)
-        a = self.attn_dropout(a)
-        if mask is not None:
-            a = a.masked_fill(mask, float('-inf'))
-        hd = torch.tensor(self.head_dim, dtype=torch.float32)
-        a = torch.softmax(a / torch.sqrt(hd), dim=-1)
-        output =  (a @ v).transpose(1, 2).reshape(b, l, d)
-        output = self.linear(output)
-        return self.resid_dropout(output)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
